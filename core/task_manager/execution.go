70d4c79874152439c3a7681ed56b2fa249867959
SeddikSalma <seddiksalma@gmail.com>
2024-06-01 00:39:07 +0100

    chore: ExecuteJob

--- core/task_manager/execution.go
+++ core/task_manager/execution.go
@@ -1,14 +1,18 @@
 package taskmanager
 
-import "github.com/Ein-Framework/Ein-Framework/core/domain/entity"
+import (
+	"github.com/Ein-Framework/Ein-Framework/core/domain/entity"
+)
 
 func (manager *TaskManager) ExecuteTemplate(Template entity.Template, AssesementID uint) (*entity.Task, error) {
 	_, err := manager.coreServices.AssessmentService.GetAssessmentById(AssesementID)
 	if err != nil {
 		return nil, err
 	}
+	return manager.createTask(Template, AssesementID)
+}
 
-	//create a task
+func (manager *TaskManager) createTask(Template entity.Template, AssesementID uint) (*entity.Task, error) {
 	task, err := manager.coreServices.TaskService.AddNewTask(entity.Queued, Template, AssesementID)
 	if err != nil {
 		return nil, err
@@ -21,7 +25,30 @@ func (manager *TaskManager) ExecuteTemplate(Template entity.Template, Assesement
 }
 
 func (manager *TaskManager) ExecuteJob(jobID uint, AssesementID uint) (*entity.JobExecution, error) {
-	return nil, nil
+	job, err := manager.coreServices.JobService.GetJobById(jobID)
+	if err != nil {
+		return nil, err
+	}
+	_, err = manager.coreServices.AssessmentService.GetAssessmentById(AssesementID)
+	if err != nil {
+		return nil, err
+	}
+	tasks := make([]entity.Task, 0)
+	for _, template := range job.Templates {
+		task, err := manager.createTask(template, AssesementID)
+		if err != nil {
+			//TODO : yaaaaaaaaaaa mongi delete tasks
+			return nil, err
+		}
+		tasks = append(tasks, *task)
+	}
+
+	jobExec, err := manager.coreServices.JobExecutionService.AddNewJobExecution(jobID, AssesementID, tasks, entity.Queued)
+	if err != nil {
+		//TODO : yaaaaaaaaaaa mongi delete tasks
+		return nil, err
+	}
+	return jobExec, nil
 }
 
 func (manager *TaskManager) CancelJob(jobID uint) error {
